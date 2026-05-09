0b8403c51ba658d4bfb4c227111539381380ed09
kabo2468 <28654659+kabo2468@users.noreply.github.com>
2026-05-10 06:14:57 +0900

    migrate vitest

--- test/MkPages.test.ts
+++ test/MkPages.test.ts
@@ -1,4 +1,4 @@
-import { readFileSync } from 'fs';
+import { readFileSync } from 'node:fs';
 import { MkPages, MPText } from '../src';
 
 describe('MkPages', () => {
---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    test: {
+        globals: true,
+        coverage: {
+            enabled: true,
+            provider: 'v8',
+            reporter: ['text', 'html'],
+            reportsDirectory: './coverage',
+        },
+    },
+});
